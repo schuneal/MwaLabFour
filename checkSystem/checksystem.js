b507a0bd7503ceda78049546616485d347369ad1
schuneal <sunilpaudel95@gmail.com>
2018-06-29 15:20:30 -0500

    observable

--- checkSystem/checksystem.js
+++ checkSystem/checksystem.js
@@ -1,26 +1,48 @@
 const os = require('os');
+const {of} = require('rxjs');
+const {filter} = require('rxjs/operators');
+
+
 var numberOfCores = os.cpus().length;
 var totalMemory = os.totalmem;
 
-var myPromise = new Promise(function(resolve, reject){
-    if(numberOfCores < 2){
-        reject("Processor is not supported!!!");
-    }
-    else if(totalMemory < 4e9){
-        reject("The app needs at least 4GB of RAM!!!")
-    }
-    else{
-        resolve("System checked successfully!!!");
-    }
-});
-
-var checkSystem = function(){
-    myPromise.then((data)=>{
-        console.log(data);
-    }).catch((err)=>{
-        console.log(err);
-    })
+var obj = {
+    'cpucore' : numberOfCores,
+    'memory' : totalMemory
 };
-checkSystem();
-console.log("Checking the system...");
+
+of(obj).pipe(
+    filter((x) => x.cpucore > 2 && x.memory > 4e9)
+)
+.subscribe(
+    (x)=>{
+        if(x){
+            console.log("System checked Successfully");
+        }else{
+            console.log("ERROR");
+        }
+    }
+)
+
+// var myPromise = new Promise(function(resolve, reject){
+//     if(numberOfCores < 2){
+//         reject("Processor is not supported!!!");
+//     }
+//     else if(totalMemory < 4e9){
+//         reject("The app needs at least 4GB of RAM!!!")
+//     }
+//     else{
+//         resolve("System checked successfully!!!");
+//     }
+// });
+
+// var checkSystem = function(){
+//     myPromise.then((data)=>{
+//         console.log(data);
+//     }).catch((err)=>{
+//         console.log(err);
+//     })
+// };
+// checkSystem();
+// console.log("Checking the system...");
 
